efc26b0c50ccfbbb3df3a3be9130ffedfaccd70b
Ashok Kumar <59107244+ashok148@users.noreply.github.com>
2023-04-13 21:43:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = 'f9b427f6-508c-4c1c-a635-73b71e6db650';
+// const projectID = 'f9b427f6-508c-4c1c-a635-73b71e6db650';
+const projectID = '06b12ba3-a2da-42c0-8a74-3c450400e99f';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
